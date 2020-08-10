5ba011d2ee6a0eb3769ceaafed23def64cca58bc
cyber-2020 <69282388+cyber-2020@users.noreply.github.com>
2020-08-09 23:00:29 -0500

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,22 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package assignment3;
+
+/**
+ *
+ * @author K******
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args)
+    {
+        CarInformation myWindow = new CarInformation();
+    }
+
+}
